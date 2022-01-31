8426540b13997ba755fb2f719694a2dbeace0d28
Wojtek <wojciechdebsqi@gmail.com>
2022-01-31 22:50:10 +0100

    Prepare initial environment

--- work_env/Scripts/activate.bat
+++ work_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\cdv-repositories\flask-gym-app\work_env
+set VIRTUAL_ENV=C:\Users\Wojtek\Documents\Studia\CDV - Informatyka\III rok, I semestr\Projektowanie aplikacji internetowych\Laboratoria\flask-gym-app\test_venv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(work_env) %PROMPT%
+set PROMPT=(test_venv) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,6 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(work_env) 
 
 :END
 if defined _OLD_CODEPAGE (
