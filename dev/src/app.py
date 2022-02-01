482a8df194206c6080454e3de9bd6dfdc9a77436
Wojtek <wojciechdebsqi@gmail.com>
2022-02-01 16:56:06 +0100

    Add templates and simple routing

--- dev/src/app.py
+++ dev/src/app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello Flask :)'
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/show_trainings')
+def show_trainings():
+    return render_template('show_trainings.html')
+
+@app.route('/add_training')
+def add_training():
+    return render_template('add_training.html')
+
+@app.route('/add_exercise')
+def add_exercise():
+    return render_template('add_exercise.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
